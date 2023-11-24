3f59b986403d7e6a455809b4d71e5757dce80bdb
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-11-24 14:13:23 +0200

    Added words list and generate random word.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-function App() {
+import { useState, useEffect } from 'react';
 
-  return (
-    <>
-      <p>Works</p>
-    </>
-  );
+import words from './wordsList.json';
+
+function App() {
+    const [wordToGuess, setWordToGuess] = useState<string>('');
+    // const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+    useEffect(() => {
+        const word = words[Math.floor(Math.random() * words.length)];
+        setWordToGuess(word);
+    }, []);
+
+    console.log(wordToGuess);
+    
+    return (
+        <>
+            <p>Works</p>
+        </>
+    );
 }
 
 export default App;
